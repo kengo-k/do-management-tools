d4a2a30df399bcace4c65e058af92169bc1c9eaa
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-06-15 21:30:50 +0900

    show droplets id list

--- functions/packages/do-management/stop-droplets/go.mod
+++ functions/packages/do-management/stop-droplets/go.mod
@@ -1,3 +1,16 @@
 module github.com/kengo-k/do-management-tools
 
 go 1.20
+
+require (
+	github.com/digitalocean/godo v1.118.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- functions/packages/do-management/stop-droplets/handler.go
+++ functions/packages/do-management/stop-droplets/handler.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/digitalocean/godo"
+)
+
 func Main(args map[string]interface{}) map[string]interface{} {
-	name, ok := args["name"].(string)
-	if !ok {
-		name = "stranger"
+	token := os.Getenv("DIGITALOCEAN_ACCESS_TOKEN")
+	if token == "" {
+		fmt.Println("Error: DIGITALOCEAN_ACCESS_TOKEN environment variable is not set.")
+		os.Exit(1)
+	}
+
+	client := godo.NewFromToken(token)
+	ctx := context.TODO()
+
+	droplets, _, err := client.Droplets.List(ctx, nil)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, droplet := range droplets {
+		fmt.Printf("Droplet ID: %d, Name: %s, Status: %s\n", droplet.ID, droplet.Name, droplet.Status)
 	}
 	msg := make(map[string]interface{})
-	msg["body"] = "Hello " + name + "!!!"
+	msg["body"] = "Hello World!"
 	return msg
 }
